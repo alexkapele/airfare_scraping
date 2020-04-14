9c911600d6bb10f71df52e31bffee4a0d336f450
alexkapele <alex.kapele@gmail.com>
2020-04-15 00:58:21 +0100

    Update scraper

--- google_flights_scraper.py
+++ google_flights_scraper.py
@@ -1,4 +1,4 @@
-def get_google_flights_data(origin,dest,date,currency,non_stop=True,save_csv=True):
+def get_google_flights_data(origin,dest,dates,currency,non_stop=True,save_csv=True,headless=True):
 
     import os
     from selenium import webdriver
@@ -23,7 +23,6 @@ def get_google_flights_data(origin,dest,date,currency,non_stop=True,save_csv=Tru
             "safebrowsing_for_trusted_sources_enabled": False,
             "safebrowsing.enabled": False})
     
-    headless = True
     
     if headless == True:
         chrome_options.add_argument('--headless')
@@ -31,79 +30,85 @@ def get_google_flights_data(origin,dest,date,currency,non_stop=True,save_csv=Tru
     
     driver = webdriver.Chrome(executable_path=os.getcwd()+"/chromedriver.exe",desired_capabilities=caps,chrome_options=chrome_options)
     
-    #Search parameters
-    #origin = 'ATH'
-    #dest = 'SKG'
-    #date = '2020-06-15'
-    #currency = 'GBP'
-    #non_stop = True
-    
+   
     if non_stop:
         params = 'e:1;s:0;sd:1;t:f;tt:o' #url end if non-stop is selected
     else:
         params = ';e:1;sd:1;t:f;tt:o' #url end if non-stop isn't selected
     
-    #Load website
-    url = 'https://www.google.com/flights?hl=en' + '#flt=' + origin + '.' + dest + '.' + date + ';c:' + currency + params
-    driver.get(url)
-    time.sleep(5)
-    
-    soup=BeautifulSoup(driver.page_source, 'lxml')
-    
-    #Extract airline names
-    airlines = []
-    for airline in soup.find_all('span', attrs={'class': 'gws-flights__ellipsize'}):
-        airlines.append(airline.getText())
-    airlines = airlines[::2]
-    airlines
-    
-    #Extract times
-    times = []
-    for time_ in soup.find_all('div', attrs={'class': 'gws-flights-results__times flt-subhead1'}):
-        times.append(time_.getText()[2:-3])
-    times
-    
-    #Extract prices (cheapest and other)
-    prices = []
-    for price in soup.find_all('div', attrs={'class': 'flt-subhead1 gws-flights-results__price gws-flights-results__cheapest-price'}) :
-        prices.append(price.getText()[6:-3])
-    
-    for price in soup.find_all('div', attrs={'class': 'flt-subhead1 gws-flights-results__price'}) :
-        prices.append(price.getText()[6:-3])
-    prices = prices[::2]
-    prices
-    
-    #Create dataframe to store data
+    #Create dataframes to store data for the date and all dates combined
     data = pd.DataFrame()
     
-    #Store extracted values
-    data['Airline'] = airlines
-    data['Time'] = times
-    data['Price'] = prices
     
-    #Separate dep and arr times
-    data['Dep Time (local)'] = data['Time'].str.split('–').str[0]
-    data['Arr Time (local)'] = data['Time'].str.split('–').str[1]
-    data['Arr Time (local)'] = data['Arr Time (local)'].str.strip()
-    data = data.drop(['Time'], axis=1)
-    
-    #Seperate price and currency
-    #data['Currency'] = data['Price'].apply(lambda x: x[:re.search(r"\d", x).start()])
-    data['Currency'] = currency
-    data['Price'] = data['Price'].apply(lambda x: x[re.search(r"\d", x).start():])
-    
-    #Add Other info
-    data['Origin'] = origin
-    data['Destination'] = dest
-    data['Flight Date'] = date
-    data['Flight Date'] = pd.to_datetime(data['Flight Date'])
-    data['Weekday'] = data['Flight Date'].dt.weekday_name
-    data['Fare Extraction Date'] = datetime.today().strftime('%Y-%m-%d')
-    data['Fare Extraction Date'] = pd.to_datetime(data['Fare Extraction Date'])
-    
-    #Reorder columns
-    data = data[['Origin','Destination','Airline','Flight Date','Weekday','Dep Time (local)', 'Arr Time (local)', 'Price', 'Currency', 'Fare Extraction Date']]
-    
+    for i, date in enumerate(dates):
+        #Load website
+        url = 'https://www.google.com/flights?hl=en' + '#flt=' + origin + '.' + dest + '.' + date + ';c:' + currency + params
+        driver.get(url)
+        time.sleep(5)
+        
+        soup=BeautifulSoup(driver.page_source, 'lxml')
+        
+        #Extract airline names
+        airlines = []
+        for airline in soup.find_all('span', attrs={'class': 'gws-flights__ellipsize'}):
+            airlines.append(airline.getText())
+        airlines = airlines[::2]
+        airlines
+        
+        #Extract times
+        times = []
+        for time_ in soup.find_all('div', attrs={'class': 'gws-flights-results__times flt-subhead1'}):
+            times.append(time_.getText()[2:-3])
+        times
+        
+        #Extract prices (cheapest and other)
+        prices = []
+        for price in soup.find_all('div', attrs={'class': 'flt-subhead1 gws-flights-results__price gws-flights-results__cheapest-price'}) :
+            prices.append(price.getText()[6:-3])
+        
+        for price in soup.find_all('div', attrs={'class': 'flt-subhead1 gws-flights-results__price'}) :
+            prices.append(price.getText()[6:-3])
+        prices = prices[::2]
+        prices
+        
+        
+        data_date = pd.DataFrame()
+        
+        if airlines: #check if there are any flights on the selected date
+            
+            #Store extracted values
+            data_date['Airline'] = airlines
+            data_date['Time'] = times
+            data_date['Price'] = prices
+            
+            #Separate dep and arr times
+            data_date['Dep Time (local)'] = data_date['Time'].str.split('–').str[0]
+            data_date['Arr Time (local)'] = data_date['Time'].str.split('–').str[1]
+            data_date['Arr Time (local)'] = data_date['Arr Time (local)'].str.strip()
+            data_date = data_date.drop(['Time'], axis=1)
+            
+            #Seperate price and currency
+            data_date['Currency'] = currency
+            data_date['Price'] = data_date['Price'].apply(lambda x: x[re.search(r"\d", x).start():])
+            
+            #Add Other info
+            data_date['Origin'] = origin
+            data_date['Destination'] = dest
+            data_date['Flight Date'] = date
+            data_date['Flight Date'] = pd.to_datetime(data_date['Flight Date'])
+            data_date['Weekday'] = data_date['Flight Date'].dt.weekday_name
+            data_date['Fare Extraction Date'] = datetime.today().strftime('%Y-%m-%d')
+            data_date['Fare Extraction Date'] = pd.to_datetime(data_date['Fare Extraction Date'])
+            
+            #Reorder columns
+            data_date = data_date[['Origin','Destination','Airline','Flight Date','Weekday','Dep Time (local)', 'Arr Time (local)', 'Price', 'Currency', 'Fare Extraction Date']]
+            
+            #Append date's results to data
+            data = data.append(data_date, ignore_index=True)
+            
+            #Print progress indicator 
+            print(str(i+1) + '/' + str(len(dates)) + ' days scraped')
+            
     #Save data
     if save_csv:
         filename = origin + '-' + dest + '_' + 'fares.csv'
