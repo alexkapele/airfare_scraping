31289cbcfb3f6a879336fd467150efd1a81fac0d
alexkapele <alex.kapele@gmail.com>
2020-04-17 21:43:50 +0100

    Upadted scraper

--- google_flights_scraper.py
+++ google_flights_scraper.py
@@ -29,8 +29,14 @@ def get_google_flights_data(origin,dest,dates,currency,non_stop=True,save_csv=Tr
         chrome_options.add_argument('--disable-gpu')
     
     driver = webdriver.Chrome(executable_path=os.getcwd()+"/chromedriver.exe",desired_capabilities=caps,chrome_options=chrome_options)
+  
+    #Get code corresponding to city when all city airports are selected
+    multiple_airports = {'London':'/m/04jpl', 'Paris':'/m/05qtj', 'New York':'/m/02_286', 'Rome':'/m/06c62'}  
+    if len(origin)>3:
+        origin = multiple_airports[origin]
+    if len(dest)>3:
+        dest = multiple_airports[dest]
     
-   
     if non_stop:
         params = 'e:1;s:0;sd:1;t:f;tt:o' #url end if non-stop is selected
     else:
@@ -53,6 +59,11 @@ def get_google_flights_data(origin,dest,dates,currency,non_stop=True,save_csv=Tr
         for airline in soup.find_all('span', attrs={'class': 'gws-flights__ellipsize'}):
             airlines.append(airline.getText())
         airlines = airlines[::2]
+        
+        #Extract Dep and Arr airports
+        airports = []
+        for airport_pair in soup.find_all('div', attrs={'class': 'gws-flights-results__airports flt-caption'}):
+            airports.append(airport_pair.getText().strip())
         
         #Extract times
         times = []
@@ -79,8 +90,14 @@ def get_google_flights_data(origin,dest,dates,currency,non_stop=True,save_csv=Tr
             
             #Store extracted values
             data_date['Airline'] = airlines
+            data_date['Airport Pair'] = airports 
             data_date['Time'] = times
             data_date['Price'] = prices
+            
+            #Separate dep and arr airports
+            data_date['Origin'] = data_date['Airport Pair'].str.split('–').str[0]
+            data_date['Destination'] = data_date['Airport Pair'].str.split('–').str[1]
+            data_date = data_date.drop(['Airport Pair'], axis=1)
             
             #Separate dep and arr times
             data_date['Dep Time (local)'] = data_date['Time'].str.split('–').str[0]
@@ -89,8 +106,6 @@ def get_google_flights_data(origin,dest,dates,currency,non_stop=True,save_csv=Tr
             data_date = data_date.drop(['Time'], axis=1)
                         
             #Add other info
-            data_date['Origin'] = origin
-            data_date['Destination'] = dest
             data_date['Flight Date'] = date
             data_date['Flight Date'] = pd.to_datetime(data_date['Flight Date'])
             data_date['Weekday'] = data_date['Flight Date'].dt.weekday_name
@@ -106,9 +121,10 @@ def get_google_flights_data(origin,dest,dates,currency,non_stop=True,save_csv=Tr
             
         #Print progress indicator 
         print(str(i+1) + '/' + str(len(dates)) + ' days scraped')
-            
+        
     #Save data
     if save_csv:
+        origin = list(multiple_airports.keys())[list(multiple_airports.values()).index(origin)]
         filename = origin + '-' + dest + '_' + 'fares.csv'
         if os.path.exists(os.getcwd()+'/Downloads/' + filename):
             data_full = pd.read_csv(os.getcwd()+'/Downloads/' + filename)
